5b6863114dd60324692a736762f15f059f25c605
Yuki Takae <s236146@wakayama-u.ac.jp>
2020-07-29 14:11:39 +0900

    お詫びの問題追加

---
+++ app/src/main/java/com/example/bmq/ApologizeActivity.java
@@ -0,0 +1,170 @@
+package com.example.bmq;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class ApologizeActivity extends AppCompatActivity
+{
+
+    private TextView countLabel;
+    private TextView questionLabel;
+    private Button answerBtn1;
+    private Button answerBtn2;
+    private Button answerBtn3;
+    private Button answerBtn4;
+
+    private String rightAnswer;
+    private int rightAnswerCount = 0;
+    private int quizCount = 1;
+    static final private int QUIZ_COUNT = 3;
+
+    private int randomNum = 0;
+
+    ArrayList<ArrayList<String>> quizArray = new ArrayList<>();
+    ArrayList<String> explanationArray = new ArrayList<>();
+
+    String quizData[][] = {
+            // {"都道府県名", "正解", "選択肢１", "選択肢２", "選択肢３"}
+            {"(社内)この度は、発送の報告が遅れてしまい、○○○○。", "申し訳ございません", "申し訳ありません", "ごめんなさい", "すみません"},
+
+            {"(社外)この度はこちらの不手際で佐藤様にご迷惑をおかけしたこと、○○○○。", "心より深くお詫び申し上げます", "申し訳ありません", "申し訳ございません", "すみません"}
+
+            , {"今回は本当に申し訳ございませんでした。このようなことが再び起こらぬよう、○○○○。", "社内全体で改善に努めます", "日々精進します", "頑張ります", "勉強しなおします"}
+
+            , {"お疲れ様です。佐藤浩二です。○○○○。このたび、お預かりしていた新企画の資料を、取引先への移動中に紛失してしまいました。", "取り急ぎ、ご報告があります", "緊急の報告です", "急ぎの連絡です", "やばいです"}
+            ,};
+
+    //解説データ、クイズデータと配列番号は対応してる
+    String explanationData[] = {"社内でのお詫びは、「申し訳ございません」で問題ないです", "社外の方へは、社内よりも深いお詫びの気持ちを表しましょう。", "社内全体で事態の重大さを認識していることを表す表現です。", "上司などに緊急の報告をする際は、お詫びよりも先に起こったことを報告します。「取り急ぎ」という言葉がよく使われます。"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ここから追加
+        countLabel = findViewById(R.id.countLabel);
+        questionLabel = findViewById(R.id.questionLabel);
+        answerBtn1 = findViewById(R.id.answerBtn1);
+        answerBtn2 = findViewById(R.id.answerBtn2);
+        answerBtn3 = findViewById(R.id.answerBtn3);
+        answerBtn4 = findViewById(R.id.answerBtn4);
+
+        // quizDataからクイズ出題用のquizArrayを作成する
+        for (int i = 0; i < quizData.length; i++)
+        {
+            // 新しいArrayListを準備
+            ArrayList<String> tmpquizarray = new ArrayList<>();
+
+
+            // クイズデータを追加
+            tmpquizarray.add(quizData[i][0]);  // 都道府県名
+            tmpquizarray.add(quizData[i][1]);  // 正解
+            tmpquizarray.add(quizData[i][2]);  // 選択肢１
+            tmpquizarray.add(quizData[i][3]);  // 選択肢２
+            tmpquizarray.add(quizData[i][4]);  // 選択肢３
+
+            explanationArray.add(explanationData[i]);//解説の保存（explanationArrayに解説が入ってる）
+
+            // tmpquizarrayをquizArrayに追加する
+            quizArray.add(tmpquizarray);
+
+        }
+        showNextQuiz();
+    }
+
+
+    public void showNextQuiz()
+    {
+        // クイズカウントラベルを更新
+        countLabel.setText("Q" + quizCount);
+
+        // ランダムな数字を取得
+        Random random = new Random();
+        randomNum = random.nextInt(quizArray.size());
+
+        // randomNumを使って、quizArrayからクイズを一つ取り出す
+        ArrayList<String> quiz = quizArray.get(randomNum);
+
+        // 問題文（都道府県名）を表示
+        questionLabel.setText(quiz.get(0));
+
+        // 正解をrightAnswerにセット
+        rightAnswer = quiz.get(1);
+
+        // クイズ配列から問題文（都道府県名）を削除
+        quiz.remove(0);
+
+        // 正解と選択肢３つをシャッフル
+        Collections.shuffle(quiz);
+
+        // 回答ボタンに正解と選択肢３つを表示
+        answerBtn1.setText(quiz.get(0));
+        answerBtn2.setText(quiz.get(1));
+        answerBtn3.setText(quiz.get(2));
+        answerBtn4.setText(quiz.get(3));
+
+        // このクイズをquizArrayから削除
+        quizArray.remove(randomNum);
+    }
+
+    public void checkAnswer(View view)
+    {
+
+        // どの回答ボタンが押されたか
+        Button answerBtn = findViewById(view.getId());
+        String btnText = answerBtn.getText().toString();
+
+        String explanation = explanationArray.get(randomNum);
+
+        String alertTitle;
+        if (btnText.equals(rightAnswer))
+        {
+            alertTitle = "正解!";
+            rightAnswerCount++;
+        }
+        else
+        {
+            alertTitle = "不正解...";
+        }
+
+        // ダイアログを作成
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(alertTitle);
+        builder.setMessage("答え : " + rightAnswer + "\n" + explanation);//解説の表示に成功
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i)
+            {
+                if (quizCount == QUIZ_COUNT)
+                {
+                    // 結果画面へ移動
+                    Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                    intent.putExtra("RIGHT_ANSWER_COUNT", rightAnswerCount);
+                    intent.putExtra("QUIZ_ALLCOUNT", QUIZ_COUNT);
+                    startActivity(intent);
+                }
+                else
+                {
+                    quizCount++;
+                    showNextQuiz();
+                }
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
+    }
+
+}
