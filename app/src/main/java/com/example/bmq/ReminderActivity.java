ca978c304750062e39df92e29cc2a4d33d1c7ceb
Yuki Takae <s236146@wakayama-u.ac.jp>
2020-08-09 11:17:30 +0900

    催促の問題で間違えた問題をデータベースに追加

--- app/src/main/java/com/example/bmq/ReminderActivity.java
+++ app/src/main/java/com/example/bmq/ReminderActivity.java
@@ -1,7 +1,9 @@
 package com.example.bmq;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +17,8 @@ import java.util.Random;
 
 public class ReminderActivity extends AppCompatActivity
 {
+    private SQLiteDatabase db;
+    private OpenHelper helper;
     private TextView countLabel;
     private TextView questionLabel;
     private Button answerBtn1;
@@ -29,10 +33,11 @@ public class ReminderActivity extends AppCompatActivity
 
     private int randomNum = 0;
 
-    ArrayList<ArrayList<String>> quizArray = new ArrayList<>();
-    ArrayList<String> explanationArray = new ArrayList<>();
+    private ArrayList<ArrayList<String>> quizArray = new ArrayList<>();
+    private ArrayList<String> explanationArray = new ArrayList<>();
+    private ArrayList<String> quiz;
 
-    String quizData[][] = {
+    private String[][] quizData = {
             // {"都道府県名", "正解", "選択肢１", "選択肢２", "選択肢３"}
             {"○○○○、ご提案書をメールでお送りいたしましたが、ご覧になりましたでしょうか。", "△月×日に", "以前", "この前", "昔々"},
 
@@ -47,7 +52,7 @@ public class ReminderActivity extends AppCompatActivity
     };
 
     //解説データ、クイズデータと配列番号は対応してる
-    String explanationData[] =
+    private String[] explanationData =
             {"いつの提案書なのかをしっかり書いておきましょう。", "2.相手に早く送ってほしい時でも、丁寧な言葉遣いで催促しましょう", "納品物の催促はこちらの状況を伝えましょう。相手がすでに発送している場合もあります。", "もし行き違いが起こった場合、相手に不快感を与えてしまうこともあるので、この文を加えて、トラブルが起こらないようにしましょう。"};
 
 
@@ -99,7 +104,7 @@ public class ReminderActivity extends AppCompatActivity
         randomNum = random.nextInt(quizArray.size());
 
         // randomNumを使って、quizArrayからクイズを一つ取り出す
-        ArrayList<String> quiz = quizArray.get(randomNum);
+        quiz = quizArray.get(randomNum);
 
         // 問題文（都道府県名）を表示
         questionLabel.setText(quiz.get(0));
@@ -141,6 +146,19 @@ public class ReminderActivity extends AppCompatActivity
         else
         {
             alertTitle = "不正解...";
+            if (helper == null)
+            {
+                helper = new OpenHelper(getApplicationContext());
+            }
+
+            if (db == null)
+            {
+                db = helper.getWritableDatabase();
+            }
+
+            quiz = quizArray.get(randomNum);
+//          quiz(0)が問題、quiz(1)正解,quiz(2)選択肢,quiz(3)選択肢,quiz(4)選択肢
+            insertData(db, quiz.get(0), quiz.get(1), quiz.get(2), quiz.get(3), quiz.get(4), explanation);
         }
 
         // ダイアログを作成
@@ -169,5 +187,17 @@ public class ReminderActivity extends AppCompatActivity
         });
         builder.setCancelable(false);
         builder.show();
+    }
+
+    private void insertData(SQLiteDatabase db, String quiz0, String quiz1, String quiz2, String quiz3,String quiz4,String EXquiz){
+
+        ContentValues values = new ContentValues();
+        values.put("quiz0", quiz0);
+        values.put("quiz1", quiz1);
+        values.put("quiz2", quiz2);
+        values.put("quiz3", quiz3);
+        values.put("quiz4", quiz4);
+        values.put("explanation", EXquiz);
+        db.insert("quizarray", null, values);
     }
 }
